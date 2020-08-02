9ea911fc3bfc94320695858511048f77a2749700
DevonTheFloor <devonthefloor@free.fr>
2020-08-02 10:19:31 +0200

    animheader

--- DevOTF/JS/dotf.js
+++ DevOTF/JS/dotf.js
@@ -1,10 +1,32 @@
-
-
-
 	
 let vsKreol = document.getElementById("kreol");
 vsKreol.addEventListener("click",function vsKreol(){
  console.log("OTTTTTTEEEEE !!");}
 );
 
+const ratio =0.5;
+var options = {
+  root: null,//document.querySelector('#observed'),
+  rootMargin: '0px',
+  threshold: ratio
+  }
 
+  var voir = function(entries, observer) {
+    entries.forEach(entry => {
+      if(entry.intersectionRatio > ratio)
+     { console.log('VISIBLE');
+     //observer.unobserve(entry.target)
+     entry.target.classList.add('visible');
+     }else{
+       console.log('INVISIBLE')
+     }
+    }); 
+  };
+
+var observer = new IntersectionObserver(voir, options);
+
+var target = document.querySelector('#target');
+observer.observe(target);
+
+
+//window.addEventListener('mousemove', e => console.log(e));
--- DevOTF/menu.js
+++ DevOTF/menu.js
@@ -1,17 +1,19 @@
 let menu = document.getElementById('menu');
 
-/*const pages =[
-    ['Accueil','"index.html" class="btn-menu"'],
-    ['CV','"./cv/index.html" target="_blank"'],
-    ['Git','"https://github.com/DevonTheFloor" target="blank" class="btn-menu"'],
-    ['Formation','"formation.html" class="btn-menu"'],
-    ['Contact','"contact.html" class="btn-menu"']
-]
-
-for(let i=0; i<pages.length;i++)
-{
-    menu.innerHTML+='<ul><a href='+pages[i][1]+'><li>'+pages[i][0]+'></li><a href='+pages[i][1]+'><li>'+pages[i][0]+'></li></ul>';
-}*/
-
  
- menu.insertAdjacentHTML('afterbegin','<ul><a href="index.html" class="btn-menu"><li>Accueil</li></a><a href="cv/index.html" target="_blank" class="btn-menu"><li> CV </li></a></ul><ul><a href="https://github.com/DevonTheFloor" target="blank" class="btn-menu"><li>GitHub</li></a><a href="formation.html" class="btn-menu"><li>Formation</li></a></ul><ul><a href="ressource.html" class="btn-menu"><li>Ressources</li></a><a href="contact2.html" class="btn-menu"><li>Contact</li></a></ul>');
+ menu.insertAdjacentHTML('afterbegin','<ul><a onclick="linked(\'http://66sono.free.fr\')" class="btn-menu"><li>Accueil</li></a><a href="cv/index.html" target="_blank" class="btn-menu"><li> CV </li></a></ul><ul><a href="https://github.com/DevonTheFloor" target="blank" class="btn-menu"><li><i class="fab fa-github"></i>GitHub</li></a><a href="formation.html" class="btn-menu"><li>Formation</li></a></ul><ul><a href="ressource.html" class="btn-menu"><li>Ressources</li></a><a href="contact2.html" class="btn-menu"><li>Contact</li></a></ul>');
+
+ function affMenu(){
+	 let ul = document.createElement('ul');
+
+	 menu.appendChild(ul);
+ }
+
+ function linked(url){
+	 let body  = document.getElementById('bodid');
+	 //body.style.backgroundColor="blue";
+	 console.log('c\'est ti part')
+	setTimeout(function(){
+		window.location.assign(url);
+	},2000);
+}
