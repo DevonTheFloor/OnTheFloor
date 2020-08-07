2a455a25813c7322eb3cd8bb2a5345ad78f1cdff
DevonTheFloor <devonthefloor@free.fr>
2020-08-07 16:51:32 +0200

    divers margin

--- DevOTF/menu.js
+++ DevOTF/menu.js
@@ -1,7 +1,7 @@
 let menu = document.getElementById('menu');
 
  
- menu.insertAdjacentHTML('afterbegin','<ul><a onclick="linked(\'http://66sono.free.fr\')" class="btn-menu"><li>Accueil</li></a><a href="cv/index.html" target="_blank" class="btn-menu"><li> CV </li></a></ul><ul><a href="https://github.com/DevonTheFloor" target="blank" class="btn-menu"><li><i class="fab fa-github"></i>GitHub</li></a><a href="formation.html" class="btn-menu"><li>Formation</li></a></ul><ul><a href="ressource.html" class="btn-menu"><li>Ressources</li></a><a href="contact2.html" class="btn-menu"><li>Contact</li></a></ul>');
+ menu.insertAdjacentHTML('afterbegin','<ul><a onclick="linked(\'index.html\')" class="btn-menu"><li>Accueil</li></a><a href="cv/index.html" target="_blank" class="btn-menu"><li> CV </li></a></ul><ul><a href="https://github.com/DevonTheFloor" target="blank" class="btn-menu"><li><i class="fab fa-github"></i>GitHub</li></a><a href="formation.html" class="btn-menu"><li>Formation</li></a></ul><ul><a href="ressource.html" class="btn-menu"><li>Ressources</li></a><a href="contact2.html" class="btn-menu"><li>Contact</li></a></ul>');
 
  function affMenu(){
 	 let ul = document.createElement('ul');
